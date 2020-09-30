9e97a2eb7c80bbc2bd80d6dfa5e8d9b858489022
LuzSallietti <luzsallietti@gmail.com>
2020-09-30 20:18:37 -0300

    30-09 createGifo

--- js/create-Gifo.js
+++ js/create-Gifo.js
@@ -2,10 +2,17 @@
 let start_button = document.getElementById("start-button");
 const img_element = document.getElementById("img-element");
 const video_container = document.getElementById("video-container");
+let upload_details= document.getElementById("upload-details");
+let upload_icon = document.getElementById("loader");
+const download_icon = document.getElementById("mygif-dload");
+const share_icon = document.getElementById("mygif-share");
+let myGif_btns = document.getElementById("upload-btns");
+let loading_msg = document.getElementById("loading-msg");
 let H1_main_title = document.getElementById("main-title");
 let instructions_paragraph = document.getElementById("instructions");
 let one_btn = document.getElementById("one-btn");
 let two_btn = document.getElementById("two-btn");
+let three_btn = document.getElementById("three-btn");
 const text_container=document.getElementById("text-container");
 
 
@@ -21,6 +28,50 @@ let is_recording = false;
 
 let gifId;
 let gifUrl;
+
+let seconds = document.getElementById("seconds");
+
+let form;
+let src;
+let blob;
+let contador=0;
+let clock = function(){
+  contador=contador+1;                
+  seconds.innerText=contador;      
+}  
+let interval;
+
+function generate_dload(url) {
+    download_icon.addEventListener("click", async () => {
+      //create new a element
+      let a = document.createElement('a');
+      // get image as blob
+      let response = await fetch(url);
+      let file = await response.blob();
+      // use download attribute https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Attributes
+      a.download = "Mi Gifo";
+      a.href = window.URL.createObjectURL(file);
+      //store download url in javascript https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
+      a.dataset.downloadurl = ['application/octet-stream', a.download, a.href].join(':');
+      //click on element to start download
+      a.click();
+  });
+}
+
+function generate_share(url){
+  share_icon.addEventListener("click",() => {
+    window.open(url)});
+}
+    
+
+function upload_succes(){
+  upload_icon.src=`./img/check.svg`
+  myGif_btns.style.visibility="visible";
+  loading_msg.innerText="GIFO subido con éxito";
+
+}
+    
+
 
 function get_cam_access(){
     H1_main_title.innerText="¿Nos das acceso a tu cámara?";
@@ -40,11 +91,12 @@ function getStreamAndRecord () {
  })
 
 
- .then(function(stream) {
-     text_container.style.visibility="hidden";
-
-    video_container.srcObject = stream;
+ .then(function(stream) { 
+          
+    video_container.srcObject = stream;    
     video_container.play()
+    H1_main_title.style.visibility="hidden";
+    instructions_paragraph.style.visibility="hidden";
     one_btn.style.backgroundColor="#fff";
     one_btn.style.color="#572ee5";
     two_btn.style.color="#fff";
@@ -69,8 +121,8 @@ function getStreamAndRecord () {
     let recorder_options = {
         video: true,
         audio: false,
-        type: "gif"
-    };
+        type: "gif",
+      };
     let camera=video_container.srcObject;
     
             
@@ -79,26 +131,54 @@ function getStreamAndRecord () {
             recorder.camera = camera;
             is_recording = true;
 
+
+    interval=setInterval(clock, 1000);    
+
     start_button.addEventListener("click", stop_recording);
  }
 
 
 function stop_recording() {
+  clearInterval(interval);
     recorder.stopRecording();
 
-    
+    start_button.removeEventListener("click", stop_recording);
+    start_button.innerText="Subir GIFO";   
                 
-    let form;
+    /*let form;
     let src;
-    let blob;
+    let blob;*/
 
-
-    recorder.camera.stop();
-    
+    recorder.camera.stop();    
     blob = recorder.getBlob();
     console.log(blob);
+    src = URL.createObjectURL(blob);
+    
+    img_element.src = src;
+
+    recorder.destroy();
+    recorder = null;
+    video_container.srcObject = null;
+
+    is_recording = false;
+
+    start_button.addEventListener("click", upload_gif);
+    
+  }
+
+function upload_gif(){
+    upload_details.style.visibility="visible";
+    start_button.removeEventListener("click",upload_gif);
+    start_button.style.visibility="hidden";
+    two_btn.style.backgroundColor="#fff";
+    two_btn.style.color="#572ee5";
+    three_btn.style.color="#fff";
+    three_btn.style.backgroundColor="#572ee5";
+    
+    
+
     form = new FormData();
-    form.append("file", blob, 'test.gif');
+    form.append("file", blob, 'mygifo.gif');
     // here is where upload happens
   
     fetch(url, {
@@ -114,27 +194,23 @@ function stop_recording() {
         .then(data => {
           gifUrl = data.data.images.original.url
           console.log(data.data.images.original.url);
+          upload_succes();
+          generate_dload(gifUrl);
+          generate_share(gifUrl);
         })
     });
   
     localStorage.setItem('misGifos', JSON.stringify(gifUrl));
   
-    src = URL.createObjectURL(blob);
-    
-    img_element.src = src;
-
-    recorder.destroy();
-    recorder = null;
-    video_container.srcObject = null;
-
-    is_recording = false;
+   
 }
+
 start_button.addEventListener("click", startCamera);
 
 function startCamera (e){
      
         if (!is_recording)
-            /*start_recording();*/
+            
             get_cam_access();
             getStreamAndRecord();
     
